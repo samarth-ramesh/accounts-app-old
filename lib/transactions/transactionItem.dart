0dc7fe04f2cbcdb5ebd88bbee33e95ed2226d165
Samarth <samarthr1@outlook.com>
2022-06-11 13:21:31 +0530

    WIP: make transaction details show

--- lib/transactions/transactionItem.dart
+++ lib/transactions/transactionItem.dart
@@ -1,14 +1,70 @@
 import 'package:accounts2/actions/getTransactions.dart';
 import 'package:flutter/material.dart';
 
+String dtToDDMMYY(DateTime dt) {
+  return "${dt.day}/${dt.month}/${dt.year}";
+}
+
+String dtToHHMM(DateTime dt) {
+  return "${dt.hour}:${dt.minute}";
+}
+
 class TransactionItem extends StatelessWidget {
   final Transaction t;
+
   const TransactionItem({Key? key, required this.t}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text(t.remarks),
+      child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(dtToDDMMYY(t.dateTime.toUtc())),
+                  Text(dtToHHMM(t.dateTime.toUtc()))
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Remarks: ",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    t.remarks,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [const Text("From"), Text(t.acc1)],
+                  ),
+                  Column(
+                    children: [const Text("Amount"), Text(t.amt.toString())],
+                  ),
+                  Column(
+                    children: [
+                      const Text("To"),
+                      Text(t.acc2),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
--- lib/transactions/transactions.dart
+++ lib/transactions/transactions.dart
@@ -38,7 +38,7 @@ class _TransactionsPageState extends State<TransactionsPage> {
         title: const Text("Transactions"),
       ),
       body: Column(
-        children: List.from(transactions.map((e) => TransactionItem(t: e,))),
+        children: List.from(transactions.map((e) => TransactionItem(t: e, key: Key(e.id.toString()),))),
       ),
     );
   }
